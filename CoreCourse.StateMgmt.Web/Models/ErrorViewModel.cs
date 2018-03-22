02f81d439838dd9082ad0819f92f2ec4f4008a72
sigged <sderdeyn@gmail.com>
2018-03-22 12:46:42 +0100

    Add project files.

---
+++ CoreCourse.StateMgmt.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CoreCourse.StateMgmt.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
